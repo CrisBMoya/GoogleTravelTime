93d11286a8f2e99011de0bfb03dcd0579bc5805a
CrisBMoya <tobal.92@gmail.com>
2017-12-15 16:33:56 -0300

    Add files via upload

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 #Server Side
 #Functions outside Shiny
-{library(RCurl)
+{
+library(RCurl)
 library(plyr)
 library(urltools)
 library(XML)
@@ -11,8 +12,9 @@ library(stringr)
 library(shiny)
 library(tableHTML)
 library(shinyjs)
-library(shinydashboard)}
-
+library(shinydashboard)
+library(mapview)
+  }
 #Decode Function
 decodeLine <- function(encoded){
   require(bitops)
@@ -117,7 +119,7 @@ server=function(input, output){
 
 output$temp=renderUI(HTML("<ul>
 <li>Enter a date and time, it must be a future date, otherwise it wont work!.</li>
-<li>Enter a Start/Origin Point in the form of 'Latitute,Longitude'.</li>
+<li>You can change the Origin Point changing the Latitude and Longitude, the mapp will follow.</li>
 <li>Choose how many points you want to calculate. 
 This value are used to create a grid of points surrounding the Start/Origin point.</li>
 <li>Enter your API Key from Google Maps API. Remember! this Key has daily limits to use it for free! 
@@ -136,115 +138,202 @@ CoordMatrix=reactive({c(OriginNorthWestv1(),OriginSouthEastv1())})
 output$debug=renderText({OriginNorthWestv1()})
 
 output$gridmap=renderLeaflet({
-  p1=leaflet() %>%  addTiles() %>% addProviderTiles(providers$OpenStreetMap)
+  p1=leaflet() %>%  addTiles() %>% addProviderTiles(providers$OpenStreetMap) %>%
+    setView(lat=input$OriginLat,
+            lng=input$OriginLon, zoom=9)
+  p1
   })
 
-gridTemp=observeEvent(input$Grid, {
-  p1=leafletProxy("gridmap") %>% clearShapes() %>% clearMarkers() %>%  addTiles() %>% addProviderTiles(providers$OpenStreetMap) %>%
+########
+#Sequence from central Y to Southest Point, in 5 steps.
+Stepsv2=reactive({round(sqrt(input$Puntos), digits=0)}) #Usar 20 es ideal
+centralSouthYv2=reactive({seq(OriginSouthEastv1()[1],OriginNorthWestv1()[1], 
+                            by=(OriginSouthEastv1()[1]-OriginNorthWestv1()[1])*-1/Stepsv2())[1:Stepsv2()]})
+
+#Sequence from central X to Westest Point, in 5 steps.
+centralWestXv2=reactive({seq(OriginSouthEastv1()[2],OriginNorthWestv1()[2], 
+                 by=(OriginSouthEastv1()[2]-OriginNorthWestv1()[2])*-1/Stepsv2())[2:(Stepsv2()+1)]})
+
+#Q3 for Quadrant 3 in cartesian system.
+GridCoords=reactive({
+
+coordinatesQ3v2=apply(expand.grid(centralSouthYv2(), centralWestXv2()), 1, paste, collapse=",")
+coordinatesQ3v2=as.data.frame(coordinatesQ3v2)
+coordinatesQ3v2=data.frame(str_split_fixed(coordinatesQ3v2$coordinatesQ3v2, ",", 2))
+coordinatesQ3v2=data.frame(lat=as.numeric(as.character(coordinatesQ3v2$X1)), lng=as.numeric(as.character(coordinatesQ3v2$X2)))
+coordinatesQ3v2
+})
+
+#######
+observeEvent(input$Grid, {
+  p1=leafletProxy("gridmap") %>% clearShapes() %>% clearMarkers() %>% removeLayersControl() %>% 
+    clearMarkers() %>% clearPopups() %>% clearControls() %>%  addTiles() %>% addProviderTiles(providers$OpenStreetMap) %>%
     setView(lat=as.numeric(strsplit(centralPointv1(),",")[[1]][1]),
             lng=as.numeric(strsplit(centralPointv1(),",")[[1]][2]),
             zoom=9)
-
+  
   p1=addRectangles(p1, lat1=CoordMatrix()[1], lng1=CoordMatrix()[2],
                    lat2=CoordMatrix()[3], lng2=CoordMatrix()[4],
-                   fillColor="transparent")
+                   fillColor="transparent", color="blue")
+  p1=addCircles(p1, lat=GridCoords()$lat, 
+                lng=GridCoords()$lng, color="black")
   p1
 })
 
 
-TravelTimeFUN=observeEvent(input$time,{
+#OUT OF OBSERVE
 
+timeVar=reactive({as.numeric(as.POSIXct(input$Date))})
+
+resultsV1=reactive({
+  resultsList <- list()
+  #Iterating Time of travel function over number of coordinates generated
+  withProgress(message="Calculating Time...", value=0, {
+    for (i in 1:nrow(GridCoords())){
+      incProgress(1/nrow(GridCoords()), detail=paste("Sample Nº", i))
+      resultsList[[i]]=as.data.frame(APIFunction(start=c(input$OriginLat,input$OriginLon), end=GridCoords()[i,], time=timeVar(), key=input$Key))
+    }
+  })
+  names(resultsList)=paste("Route",1:length(resultsList), sep="")
   
-#Sequence from central Y to Southest Point, in 5 steps.
-Steps=round(sqrt(input$Puntos), digits=0) #Usar 20 es ideal
-centralSouthY=seq(OriginSouthEastv1()[1],OriginNorthWestv1()[1], by=(OriginSouthEastv1()[1]-OriginNorthWestv1()[1])*-1/Steps)[1:Steps]
+  resultsList=resultsList[sapply(resultsList, function(x) x[1,1]<=90 & x[1,1] >= 20)]
+  resultsList
+  # colorRang=sapply(resultsList, function(x) x[1,1])
+  # colorRang=cut(colorRang, breaks=quantile(colorRang,seq(0,1, length.out=7)), include.lowest=T, labels=F)
+  # resultsList=mapply(cbind, resultsList, "colorRang"=colorRang, SIMPLIFY=FALSE)
+  # 
+  # #Formatting palletes, colors and groups for further plotting.
+  # pal=colorQuantile(c("green","red"), 1:max(unique(colorRang)))
+  # colorPallete=pal(sapply(resultsList, function(x) x$colorRang[1]))
+  # resultsList=mapply(cbind, resultsList, "colorPallete"=colorPallete, SIMPLIFY=FALSE)
+  # 
+  # 
+  # resultsList
+  })
 
-#Sequence from central X to Westest Point, in 5 steps.
-centralWestX=seq(OriginSouthEastv1()[2],OriginNorthWestv1()[2], by=(OriginSouthEastv1()[2]-OriginNorthWestv1()[2])*-1/Steps)[2:(Steps+1)]
+colorRang=reactive({
+  colorRang=sapply(resultsV1(), function(x) x[1,1])
+  colorRang=cut(colorRang, breaks=quantile(colorRang,seq(0,1, length.out=7)), include.lowest=T, labels=F)
+  colorRang 
+  })
 
-#Q3 for Quadrant 3 in cartesian system.
-coordinatesQ3=apply(expand.grid(centralSouthY, centralWestX), 1, paste, collapse=",")
-coordinatesQ3=as.data.frame(coordinatesQ3)
-coordinatesQ3=data.frame(str_split_fixed(coordinatesQ3$coordinatesQ3, ",", 2))
-coordinatesQ3=data.frame(lat=as.numeric(as.character(coordinatesQ3$X1)), lng=as.numeric(as.character(coordinatesQ3$X2)))
+results=reactive({
+  resultsV2=mapply(cbind, resultsV1(), "colorRang"=colorRang(), SIMPLIFY=FALSE)
+  #Formatting palletes, colors and groups for further plotting.
+  pal=colorQuantile(c("green","red"), 1:max(unique(colorRang())))
+  colorPallete=pal(sapply(resultsV2, function(x) x$colorRang[1]))
+  resultsV2=mapply(cbind, resultsV2, "colorPallete"=colorPallete, SIMPLIFY=FALSE)
+  resultsV2
+  })
 
-#Iterating Time of travel function over number of coordinates generated
-results <- list()
-timeVar=as.numeric(as.POSIXct(input$Date))
-
-withProgress(message="Calculating Time per each route", value=0, {
- for (i in 1:nrow(coordinatesQ3)){
-   incProgress(1/nrow(coordinatesQ3), detail=paste("Sample Nº", i))
-   results[[i]]=as.data.frame(APIFunction(start=c(input$OriginLat,input$OriginLon), end=coordinatesQ3[i,], time=timeVar, key=input$Key))
-  }
+quantileInfo=reactive({
+quantileInfo=as.data.frame(cbind(sapply(results(), function(x) x[1,1]),sapply(results(), function(x) x[1,4])))
+quantileInfo=sapply(split(quantileInfo$V1, quantileInfo$V2), mean)
+quantileInfo
 })
 
-names(results)=paste("Route",1:length(results), sep="")
+pal2=reactive({
+pal2=colorQuantile(c("green","red"), quantileInfo())
+})
 
-#Only values under certain time
-before=length(results)
-results=results[sapply(results, function(x) x[1,1]<=90 & x[1,1] >= 20)]
-colorRang=sapply(results, function(x) x[1,1])
-colorRang=cut(colorRang, breaks=quantile(colorRang,seq(0,1, length.out=7)), include.lowest=T, labels=F)
-results=mapply(cbind, results, "colorRang"=colorRang, SIMPLIFY=FALSE)
-after=length(results)
+groupName=reactive({
+  groupName=paste("Quantile nº", sort(unique(colorRang())), sep="")
+})
+##Note. Reactive is getting colorRang in between, observe needs this value. colorRan
+#should be reactive too. Two different instances of resulst as reactive will be needed, cause
+#results its also using colorRang to change or aggregate values to itself.
+observeEvent(input$time,{
 
-#Formatting palletes, colors and groups for further plotting.
-pal=colorQuantile(c("green","red"), 1:max(unique(colorRang)))
-colorPallete=pal(sapply(results, function(x) x$colorRang[1]))
-results=mapply(cbind, results, "colorPallete"=colorPallete, SIMPLIFY=FALSE)
-
-quantileInfo=as.data.frame(cbind(sapply(results, function(x) x[1,1]),sapply(results, function(x) x[1,4])))
-quantileInfo=sapply(split(quantileInfo$V1, quantileInfo$V2), mean)
-
-pal2=colorQuantile(c("green","red"), quantileInfo)
-groupName=paste("Quantile nº", sort(unique(colorRang)), sep="")
-
-#Plot
-
-p1=leafletProxy("gridmap") %>% clearShapes() %>% clearMarkers() %>% clearGroup(c(groupName,"Info")) %>% 
-  clearMarkers() %>% clearPopups() %>% clearControls() %>%
-  addTiles() %>% addLayersControl(baseGroups = c(groupName,"Info")) %>% 
-  addProviderTiles(providers$OpenStreetMap)
-for(i in 1:length(results)){
-  #Add route liens
-  p1=addPolylines(p1, lat=results[[i]]$lat,
-                  lng=results[[i]]$lon, color = paste(results[[i]]$colorPallete[1]),
-                  group=groupName[(results[[i]]$colorRang)[1]]) 
-  #Add markers         
-  p1=addMarkers(p1,label=paste(round(results[[i]]$time[1], digits=2),"min"),
-                lat=results[[i]]$lat[nrow(results[[i]])],
-                lng=results[[i]]$lon[nrow(results[[i]])],
-                group=groupName[(results[[i]]$colorRang)[1]],
-                popup=paste("Route nº", results[[i]]$route[1]))
+    #Plot
   
-
-}
-
-#Add info marker         
-p1=addMarkers(p1, group="Info", lat=input$OriginLat,
-              lng=input$OriginLon,
-              popup = paste("Nº of Points calculated: ", nrow(coordinatesQ3),"; ",
-                            "Nº of Points after filter: ", length(results),"; ",
-                            "Date and time used: ", paste(input$Date, sep=""),"; ",
-                            "Central point is HERE.", sep=""))
-
-p1= p1 %>% addLegend("bottomright", colors=pal2(quantileInfo),
-                     labels=round(quantileInfo, digits = 2),
-                     title="Average Travel Time")
-
-for(i in sort(unique(colorRang))){
-  p1=addAwesomeMarkers(p1, group=groupName[i],
-                       lat=input$OriginLat, 
-                       lng=input$OriginLon,
-                       icon = awesomeIcons(
-                         library = 'glyphicon',
-                         icon = "download",
-                         markerColor = "black"))}
-p1
-
-      })
+  p1=leafletProxy("gridmap") %>% clearShapes() %>% clearMarkers() %>% clearGroup(c(groupName(),"Info")) %>% 
+    clearMarkers() %>% clearPopups() %>% clearControls() %>%
+    addTiles() %>% addLayersControl(baseGroups = c(groupName(),"Info")) %>% 
+    addProviderTiles(providers$OpenStreetMap)
+  for(i in 1:length(results())){
+    #Add route liens
+    p1=addPolylines(p1, lat=results()[[i]]$lat,
+                    lng=results()[[i]]$lon, color = paste(results()[[i]]$colorPallete[1]),
+                    group=groupName()[(results()[[i]]$colorRang)[1]]) 
+    #Add markers         
+    p1=addMarkers(p1,label=paste(round(results()[[i]]$time[1], digits=2),"min"),
+                  lat=results()[[i]]$lat[nrow(results()[[i]])],
+                  lng=results()[[i]]$lon[nrow(results()[[i]])],
+                  group=groupName()[(results()[[i]]$colorRang)[1]],
+                  popup=paste("Route nº", results()[[i]]$route[1]))
     
+    
+  }
+  
+  #Add info marker         
+  p1=addMarkers(p1, group="Info", lat=input$OriginLat,
+                lng=input$OriginLon,
+                popup = paste("Nº of Points calculated: ", nrow(GridCoords()),"; ",
+                              "Nº of Points after filter: ", length(results()),"; ",
+                              "Date and time used: ", paste(input$Date, sep=""),"; ",
+                              "Central point is HERE.", sep=""))
+  
+  p1= p1 %>% addLegend("bottomright", colors=pal2()(quantileInfo()),
+                       labels=round(quantileInfo(), digits = 2),
+                       title="Average Travel Time")
+  
+  for(i in sort(unique(colorRang()))){
+    p1=addAwesomeMarkers(p1, group=groupName()[i],
+                         lat=input$OriginLat, 
+                         lng=input$OriginLon,
+                         icon = awesomeIcons(
+                           library = 'glyphicon',
+                           icon = "download",
+                           markerColor = "black"))}
+  p1
+  
+})
+
+mapdown=reactive({
+  
+  
+  p1=leafletProxy("gridmap") %>% clearShapes() %>% clearMarkers() %>% clearGroup(c(groupName(),"Info")) %>% 
+    clearMarkers() %>% clearPopups() %>% clearControls() %>%
+    addTiles() %>% addLayersControl(baseGroups = c(groupName(),"Info")) %>% 
+    addProviderTiles(providers$OpenStreetMap)
+  for(i in 1:length(results())){
+    #Add route liens
+    p1=addPolylines(p1, lat=results()[[i]]$lat,
+                    lng=results()[[i]]$lon, color = paste(results()[[i]]$colorPallete[1]),
+                    group=groupName()[(results()[[i]]$colorRang)[1]]) 
+    #Add markers         
+    p1=addMarkers(p1,label=paste(round(results()[[i]]$time[1], digits=2),"min"),
+                  lat=results()[[i]]$lat[nrow(results()[[i]])],
+                  lng=results()[[i]]$lon[nrow(results()[[i]])],
+                  group=groupName()[(results()[[i]]$colorRang)[1]],
+                  popup=paste("Route nº", results()[[i]]$route[1]))
+    
+    
+  }
+  
+  #Add info marker         
+  p1=addMarkers(p1, group="Info", lat=input$OriginLat,
+                lng=input$OriginLon,
+                popup = paste("Nº of Points calculated: ", nrow(GridCoords()),"; ",
+                              "Nº of Points after filter: ", length(results()),"; ",
+                              "Date and time used: ", paste(input$Date, sep=""),"; ",
+                              "Central point is HERE.", sep=""))
+  
+  p1= p1 %>% addLegend("bottomright", colors=pal2()(quantileInfo()),
+                       labels=round(quantileInfo(), digits = 2),
+                       title="Average Travel Time")
+  
+  for(i in sort(unique(colorRang()))){
+    p1=addAwesomeMarkers(p1, group=groupName()[i],
+                         lat=input$OriginLat, 
+                         lng=input$OriginLon,
+                         icon = awesomeIcons(
+                           library = 'glyphicon',
+                           icon = "download",
+                           markerColor = "black"))}
+  p1
+  
+  })
 }
 
 
--- ui.R
+++ ui.R
@@ -1,5 +1,6 @@
 #Funciones fuera de Shiny App
-{library(RCurl)
+{
+library(RCurl)
 library(plyr)
 library(urltools)
 library(XML)
@@ -10,7 +11,8 @@ library(stringr)
 library(shiny)
 library(tableHTML)
 library(shinyjs)
-library(shinydashboard)}
+library(shinydashboard)
+  }
 #Client Side
 
 ui= fluidPage(
@@ -23,26 +25,27 @@ ui= fluidPage(
   fluidRow(
     box(width = 10, title="Origin point:",
         splitLayout(
-          numericInput(inputId="OriginLat", label="Latitude", value=-33.4516906, min=-90, max=90),
-          numericInput(inputId="OriginLon", label="Longitude", value=-70.66742690000001, min=-180, max=180)
+          numericInput(inputId="OriginLat", label="Latitude", value=-33.4516906, min=-90, max=90, step=0.5),
+          numericInput(inputId="OriginLon", label="Longitude", value=-70.66742690000001, min=-180, max=180, step=0.5)
         ),
         helpText(a("Find Latitude and Longitude values for specific locations here!", target="_blank", href="https://www.gps-coordinates.net/"))
         
     )
   ),
   
-  numericInput(inputId="Puntos", label="Points", value=10, min=10, max=500),
+  
 
   fluidRow(
     box(width = 10, title="Optional: Grid size", footer="Dont change this, ideally",
         splitLayout(
-          numericInput(inputId="PointMatrixLat", label="Latitude", value=0.5154258, min=0.1, max=1),
-          numericInput(inputId="PointMatrixLon", label="Longitude", value=0.8047485, min=0.1, max=1)
+          numericInput(inputId="PointMatrixLat", label="Latitude", value=0.5, min=0.1, max=1),
+          numericInput(inputId="PointMatrixLon", label="Longitude", value=0.8, min=0.1, max=1)
         )
     )
   ),
+  numericInput(inputId="Puntos", label="Points", value=100, min=100, max=500),
   
-  textInput(inputId="Key", label="Google API Key", value=""),
+  textInput(inputId="Key", label="Google API Key"),
   helpText(a("Get your Google Maps API Key here!", target="_blank", href="https://developers.google.com/maps/documentation/javascript/get-api-key?hl=EN"))
     ),
   mainPanel(
@@ -51,10 +54,8 @@ ui= fluidPage(
   
   div(actionButton("time", "Run"), style="float:left"),
   actionButton("Grid","Plot Grid"),
-  leafletOutput("gridmap"),
-  leafletOutput("mymap")
+  leafletOutput("gridmap")
   ))
-  
   )
 
 #Ui elije Fecha, Origen, Nº de Puntos y Clave.
