379e69d83d8508720a92ed5236f124a9e37dd0d8
CrisBMoya <tobal.92@gmail.com>
2018-01-24 13:09:39 -0300

    Add files via upload

--- server.R
+++ server.R
@@ -1,122 +1,130 @@
 #Server Side
 #Functions outside Shiny
+#Libraries
+
 {
 library(RCurl)
-library(plyr)
-library(urltools)
 library(XML)
 library(leaflet)
-library(htmlwidgets)
-library(sp)
 library(stringr)
 library(shiny)
-library(tableHTML)
 library(shinyjs)
 library(shinydashboard)
-library(mapview)
   }
-#Decode Function
-decodeLine <- function(encoded){
-  require(bitops)
-  
-  vlen <- nchar(encoded)
-  vindex <- 0
-  varray <- NULL
-  vlat <- 0
-  vlng <- 0
-  
-  while(vindex < vlen){
-    vb <- NULL
-    vshift <- 0
-    vresult <- 0
-    repeat{
-      if(vindex + 1 <= vlen){
-        vindex <- vindex + 1
-        vb <- as.integer(charToRaw(substr(encoded, vindex, vindex))) - 63  
+
+#Functions
+{#Decode Function
+  decodeLine <- function(encoded){
+    require(bitops)
+    
+    vlen <- nchar(encoded)
+    vindex <- 0
+    varray <- NULL
+    vlat <- 0
+    vlng <- 0
+    
+    while(vindex < vlen){
+      vb <- NULL
+      vshift <- 0
+      vresult <- 0
+      repeat{
+        if(vindex + 1 <= vlen){
+          vindex <- vindex + 1
+          vb <- as.integer(charToRaw(substr(encoded, vindex, vindex))) - 63  
+        }
+        
+        vresult <- bitOr(vresult, bitShiftL(bitAnd(vb, 31), vshift))
+        vshift <- vshift + 5
+        if(vb < 32) break
       }
       
-      vresult <- bitOr(vresult, bitShiftL(bitAnd(vb, 31), vshift))
-      vshift <- vshift + 5
-      if(vb < 32) break
-    }
-    
-    dlat <- ifelse(
-      bitAnd(vresult, 1)
-      , -(bitShiftR(vresult, 1)+1)
-      , bitShiftR(vresult, 1)
-    )
-    vlat <- vlat + dlat
-    
-    vshift <- 0
-    vresult <- 0
-    repeat{
-      if(vindex + 1 <= vlen) {
-        vindex <- vindex+1
-        vb <- as.integer(charToRaw(substr(encoded, vindex, vindex))) - 63        
+      dlat <- ifelse(
+        bitAnd(vresult, 1)
+        , -(bitShiftR(vresult, 1)+1)
+        , bitShiftR(vresult, 1)
+      )
+      vlat <- vlat + dlat
+      
+      vshift <- 0
+      vresult <- 0
+      repeat{
+        if(vindex + 1 <= vlen) {
+          vindex <- vindex+1
+          vb <- as.integer(charToRaw(substr(encoded, vindex, vindex))) - 63        
+        }
+        
+        vresult <- bitOr(vresult, bitShiftL(bitAnd(vb, 31), vshift))
+        vshift <- vshift + 5
+        if(vb < 32) break
       }
       
-      vresult <- bitOr(vresult, bitShiftL(bitAnd(vb, 31), vshift))
-      vshift <- vshift + 5
-      if(vb < 32) break
+      dlng <- ifelse(
+        bitAnd(vresult, 1)
+        , -(bitShiftR(vresult, 1)+1)
+        , bitShiftR(vresult, 1)
+      )
+      vlng <- vlng + dlng
+      
+      varray <- rbind(varray, c(vlat * 1e-5, vlng * 1e-5))
     }
-    
-    dlng <- ifelse(
-      bitAnd(vresult, 1)
-      , -(bitShiftR(vresult, 1)+1)
-      , bitShiftR(vresult, 1)
-    )
-    vlng <- vlng + dlng
-    
-    varray <- rbind(varray, c(vlat * 1e-5, vlng * 1e-5))
+    coords <- data.frame(varray)
+    names(coords) <- c("lat", "lon")
+    coords
   }
-  coords <- data.frame(varray)
-  names(coords) <- c("lat", "lon")
-  coords
+  
+  #API Function
+  APIFunction=function(start,end,time,mode,key,debug=FALSE){
+    
+    #Base code
+    urlBase="https://maps.googleapis.com/maps/api/directions/xml?"
+    
+    #User defined
+    startPoint=as.character(paste(start, collapse = ","))
+    endPoint=as.character(paste(end, collapse=","))
+    mode="driving"
+    traffic="best_guess"
+    time=time
+    key=key
+    
+    #Creating the whole request url
+    wholeUrl=paste(urlBase,"origin=",startPoint,
+                   "&destination=",endPoint,
+                   "&mode=",mode,
+                   "&traffic_model=",traffic,
+                   "&departure_time=",time,
+                   "&key=",key, sep="")
+    
+    if(debug==TRUE){
+      return(wholeUrl)
+    }else 
+      Query1=try(RCurl::getURL(wholeUrl, .mapUnicode = FALSE))
+    Query2=grepl(pattern="error", x=Query1, ignore.case=TRUE)
+    Query3=try(xpathSApply((htmlParse(Query1)),
+                           "//directionsresponse/status", xmlValue))
+    
+    if(Query2==TRUE){
+      errorFun=0
+      names(errorFun)="Error with getURL"
+      return(c(errorFun, Query2))
+    }else if(Query3 != "OK"){
+      errorFun=1
+      names(errorFun)="Error with Google Response"
+      return(c(errorFun, Query3))
+    }else{
+      tempLine=decodeLine((xpathSApply((htmlParse(Query1)),
+                                       "//overview_polyline", xmlValue)))
+      time=as.numeric(xpathSApply((htmlParse(Query1)),
+                                  "//duration_in_traffic/value", xmlValue))/60
+      distance=max(as.numeric(xpathSApply((htmlParse(Query1)),
+                                          "//distance/value", xmlValue))/1000)
+      return(c(time, distance, tempLine, Query1))
+    }}
 }
 
-#API Function
-APIFunction=function(start,end,time,mode,key){
-  #Base code
-  urlBase="https://maps.googleapis.com/maps/api/directions/xml?"
-  
-  #User defined
-  startPoint=as.character(paste(start, collapse = ","))
-  endPoint=as.character(paste(end, collapse=","))
-  mode="driving"
-  traffic="best_guess"
-  time=time
-  key=key
-  
-  #Creating the whole request url
-  wholeUrl=paste(urlBase,"origin=",startPoint,
-                 "&destination=",endPoint,
-                 "&mode=",mode,
-                 "&traffic_model=",traffic,
-                 "&departure_time=",time,
-                 "&key=",key, sep="")
-  
-  result=RCurl::getURL(wholeUrl, .mapUnicode = FALSE)
-  result=htmlParse(result)
-  
-  {if(!xpathSApply(result, "//directionsresponse/status", xmlValue) == "OK"){
-    errorFun=1
-    names(errorFun)="time"
-    return(errorFun)
-  }else
-    
-    tempLine=decodeLine((xpathSApply(result, "//overview_polyline", xmlValue)))
-    time=as.numeric(xpathSApply(result, "//duration_in_traffic/value", xmlValue))/60
-    time=c(rep(time, dim(tempLine)[1]))
-    time=list(time)
-    names(time)="time"
-    return(c(time,tempLine))
-  }
-}
-
-
+#Beggining of shiny
 server=function(input, output){
 
-
+#Instructions
 output$temp=renderUI(HTML("<ul>
 <li>Enter a date and time, it must be a future date, otherwise it wont work!.</li>
 <li>You can change the Origin Point changing the Latitude and Longitude, the mapp will follow.</li>
@@ -127,147 +135,185 @@ So don't waste it!</li>
 </ul>"))
 
 #Calculatin matrix of points
-centralPointv1=reactive({paste(c(input$OriginLat,input$OriginLon), collapse=",")})
-latDistv1=reactive({input$PointMatrixLat/2})
-lonDistv1=reactive({input$PointMatrixLon/2})
-
-OriginNorthWestv1=reactive({c(latDistv1() + as.numeric(strsplit(centralPointv1(),",")[[1]][1]), as.numeric(strsplit(centralPointv1(),",")[[1]][2]) - lonDistv1())})
-OriginSouthEastv1=reactive({c((as.numeric(strsplit(centralPointv1(),",")[[1]][1])) - latDistv1(), lonDistv1() + as.numeric(strsplit(centralPointv1(),",")[[1]][2]))})
-CoordMatrix=reactive({c(OriginNorthWestv1(),OriginSouthEastv1())})
-
-output$debug=renderText({OriginNorthWestv1()})
-
 output$gridmap=renderLeaflet({
   p1=leaflet() %>%  addTiles() %>% addProviderTiles(providers$OpenStreetMap) %>%
-    setView(lat=input$OriginLat,
-            lng=input$OriginLon, zoom=9)
+    setView(lat=input$centralPointLat,
+            lng=input$centralPointLon, zoom=9)
   p1
   })
 
 ########
-#Sequence from central Y to Southest Point, in 5 steps.
-Stepsv2=reactive({round(sqrt(input$Puntos), digits=0)}) #Usar 20 es ideal
-centralSouthYv2=reactive({seq(OriginSouthEastv1()[1],OriginNorthWestv1()[1], 
-                            by=(OriginSouthEastv1()[1]-OriginNorthWestv1()[1])*-1/Stepsv2())[1:Stepsv2()]})
+##SET TIME
+StartingDayReac=reactive({as.numeric(as.POSIXct(as.character(input$Date), tz="UTC"))})
+TimeZoneReac=reactive({as.numeric(input$TimeZone)})
+DayTimeReac=reactive({StartingDayReac()+86400-(TimeZoneReac()*3600)})
+##SET ORIGIN
+centralPointReac=reactive({c(input$centralPointLat,input$centralPointLon)})
+##NUMERO DE PUNTOS    
+NPointsGridReac=reactive({input$Puntos})
+NPointsReac=reactive({round(sqrt(NPointsGridReac()), digits=0)})
 
-#Sequence from central X to Westest Point, in 5 steps.
-centralWestXv2=reactive({seq(OriginSouthEastv1()[2],OriginNorthWestv1()[2], 
-                 by=(OriginSouthEastv1()[2]-OriginNorthWestv1()[2])*-1/Stepsv2())[2:(Stepsv2()+1)]})
+#Custom Matrix
+#Choosing size of Point Gird relative to the central Point. 
+#X=Longitude, Horizontal; Y=Latitude, Vertical
+LongitudeAxisLengthReac=reactive({input$LongitudeAxisLength})
+LatitudeAxisLengthReac=reactive({input$LatitudeAxisLength})
 
-#Q3 for Quadrant 3 in cartesian system.
-GridCoords=reactive({
-coordinatesQ3v2=apply(expand.grid(centralSouthYv2(), centralWestXv2()), 1, paste, collapse=",")
-coordinatesQ3v2=as.data.frame(coordinatesQ3v2)
-coordinatesQ3v2=data.frame(str_split_fixed(coordinatesQ3v2$coordinatesQ3v2, ",", 2))
-coordinatesQ3v2=data.frame(lat=as.numeric(as.character(coordinatesQ3v2$X1)), lng=as.numeric(as.character(coordinatesQ3v2$X2)))
-coordinatesQ3v2
+
+#Extend Grid from central point to the outer parts. MIND THE ORDER OF FACTORS
+OriginYAxisExtendReac=reactive({c((LatitudeAxisLengthReac()/2)+as.numeric(centralPointReac()[1]),as.numeric(centralPointReac()[2])-(LongitudeAxisLengthReac()/2))})
+OriginXAxisExtendReac=reactive({c(as.numeric(centralPointReac()[1])-(LatitudeAxisLengthReac()/2),(LongitudeAxisLengthReac()/2) + as.numeric(centralPointReac()[2]))})
+
+#Generating the sequence of points, the actual Grid of points.
+
+##Sequential steps from the center to the Southest Point (Longitude, lowest point)
+SeqAlongLatitudeReac=reactive({seq(OriginXAxisExtendReac()[1],OriginYAxisExtendReac()[1], 
+                     by=(OriginXAxisExtendReac()[1]-OriginYAxisExtendReac()[1])*-1/NPointsReac())[1:NPointsReac()]})
+SeqAlongLongitudeReac=reactive({seq(OriginXAxisExtendReac()[2],OriginYAxisExtendReac()[2], 
+                      by=(OriginXAxisExtendReac()[2]-OriginYAxisExtendReac()[2])*-1/NPointsReac())[2:(NPointsReac()+1)]})
+
+CoordinateMatrixReac=reactive({
+CoordinateMatrix=apply(expand.grid(SeqAlongLatitudeReac(), SeqAlongLongitudeReac()), 1, paste, collapse=",")
+CoordinateMatrix=as.data.frame(CoordinateMatrix)
+CoordinateMatrix=data.frame(str_split_fixed(CoordinateMatrix$CoordinateMatrix, ",", 2))
+CoordinateMatrix=data.frame(lat=as.numeric(as.character(CoordinateMatrix$X1)), 
+                            lng=as.numeric(as.character(CoordinateMatrix$X2)))
 })
 
-#######
+
+
+#Plotting a rectangle and every point to be calculated by the API
 observeEvent(input$Grid, {
   p1=leafletProxy("gridmap") %>% clearShapes() %>% clearMarkers() %>% removeLayersControl() %>% 
     clearMarkers() %>% clearPopups() %>% clearControls() %>%  addTiles() %>% addProviderTiles(providers$OpenStreetMap) %>%
-    setView(lat=as.numeric(strsplit(centralPointv1(),",")[[1]][1]),
-            lng=as.numeric(strsplit(centralPointv1(),",")[[1]][2]),
+    setView(lat=as.numeric(centralPointReac()[1]),
+            lng=as.numeric(centralPointReac()[2]),
             zoom=9)
-  
-  p1=addRectangles(p1, lat1=CoordMatrix()[1], lng1=CoordMatrix()[2],
-                   lat2=CoordMatrix()[3], lng2=CoordMatrix()[4],
-                   fillColor="transparent", color="blue")
-  p1=addCircles(p1, lat=GridCoords()$lat, 
-                lng=GridCoords()$lng, color="black")
-  p1
+  p1=addCircles(p1, lat=CoordinateMatrixReac()$lat,
+                        lng=CoordinateMatrixReac()$lng, 
+                        popup=paste(1:nrow(CoordinateMatrixReac())))
+  p1=addMarkers(p1, lat=centralPointReac()[1],
+                        lng=centralPointReac()[2]) 
 })
 
+#Running API
+APIResults=reactive({
 
-#OUT OF OBSERVE
-
-timeVar=reactive({as.numeric(as.POSIXct(input$Date))})
-
-resultsV1=reactive({
-  resultsList <- list()
+  resFuture <- list()
   withProgress(message="Calculating Time...", value=0, {
-    for (i in 1:nrow(GridCoords())){
-      incProgress(1/nrow(GridCoords()), detail=paste("Sample Nº", i))
-      resultsList[[i]]=as.data.frame(APIFunction(start=c(input$OriginLat,input$OriginLon), end=GridCoords()[i,], time=timeVar(), key=input$Key))
+    for(routes in 1:nrow(CoordinateMatrixReac())){
+      incProgress(1/nrow(CoordinateMatrixReac()), detail=paste("Sample Nº", routes))
+      resFuture[[routes]]=APIFunction(start=c(centralPointReac()[1],centralPointReac()[2]), 
+                                end=CoordinateMatrixReac()[routes,], 
+                                time=DayTimeReac(), key=input$Key)
     }
-  })
-  names(resultsList)=paste("Route",1:length(resultsList), sep="")
-  resultsList=resultsList[sapply(resultsList, function(x) x[1,1]<=90 & x[1,1] >= 20)]
-  resultsList
-  })
 
-colorRang=reactive({
-  colorRang=sapply(resultsV1(), function(x) x[1,1])
-  colorRang=cut(colorRang, breaks=quantile(colorRang,seq(0,1, length.out=7)), include.lowest=T, labels=F)
-  colorRang 
+    })
+  names(resFuture)=paste("Route",1:nrow(CoordinateMatrixReac()), sep="")
+  resFuture
   })
 
-results=reactive({
-  resultsV2=mapply(cbind, resultsV1(), "colorRang"=colorRang(), SIMPLIFY=FALSE)
-  pal=colorQuantile(c("green","red"), 1:max(unique(colorRang())))
-  colorPallete=pal(sapply(resultsV2, function(x) x$colorRang[1]))
-  resultsV2=mapply(cbind, resultsV2, "colorPallete"=colorPallete, SIMPLIFY=FALSE)
-  resultsV2
-  })
+#Filtering Errors
+DayListReac=reactive({lapply(APIResults(), function(x) x[length(x)==5])})
 
-quantileInfo=reactive({
-quantileInfo=as.data.frame(cbind(sapply(results(), function(x) x[1,1]),sapply(results(), function(x) x[1,4])))
-quantileInfo=sapply(split(quantileInfo$V1, quantileInfo$V2), mean)
-quantileInfo
-})
+#Renaming all data
+DayListv2Reac=reactive({lapply(DayListReac(), function(x){names(x)=c("time","distance","lat","lon","xml"); return(x)})})
 
-pal2=reactive({
-pal2=colorQuantile(c("green","red"), quantileInfo())
-})
+TimeStoreReac=reactive({unlist(lapply(DayListv2Reac(), function(x) x$time))})
+Breaks=4
+TimeRangeReac=reactive({(max(TimeStoreReac())-min(TimeStoreReac()))/Breaks})
 
-groupName=reactive({
-  groupName=paste("Quantile nº", sort(unique(colorRang())), sep="")
-})
 
-observeEvent(input$time,{
-  p1=leafletProxy("gridmap") %>% clearShapes() %>% clearMarkers() %>% clearGroup(c(groupName(),"Info")) %>% 
-    clearMarkers() %>% clearPopups() %>% clearControls() %>%
-    addTiles() %>% addLayersControl(baseGroups = c(groupName(),"Info")) %>% 
-    addProviderTiles(providers$OpenStreetMap)
-  for(i in 1:length(results())){
-    #Add route lines
-    p1=addPolylines(p1, lat=results()[[i]]$lat,
-                    lng=results()[[i]]$lon, color = paste(results()[[i]]$colorPallete[1]),
-                    group=groupName()[(results()[[i]]$colorRang)[1]]) 
-    #Add markers         
-    p1=addMarkers(p1,label=paste(round(results()[[i]]$time[1], digits=2),"min"),
-                  lat=results()[[i]]$lat[nrow(results()[[i]])],
-                  lng=results()[[i]]$lon[nrow(results()[[i]])],
-                  group=groupName()[(results()[[i]]$colorRang)[1]],
-                  popup=paste("Route nº", results()[[i]]$route[1]))
-    
-    
-  }
-  
-  #Add info marker         
-  p1=addMarkers(p1, group="Info", lat=input$OriginLat,
-                lng=input$OriginLon,
-                popup = paste("Nº of Points calculated: ", nrow(GridCoords()),"; ",
-                              "Nº of Points after filter: ", length(results()),"; ",
-                              "Date and time used: ", paste(input$Date, sep=""),"; ",
-                              "Central point is HERE.", sep=""))
-  
-  p1= p1 %>% addLegend("bottomright", colors=pal2()(quantileInfo()),
-                       labels=round(quantileInfo(), digits = 2),
-                       title="Average Travel Time")
-  
-  for(i in sort(unique(colorRang()))){
-    p1=addAwesomeMarkers(p1, group=groupName()[i],
-                         lat=input$OriginLat, 
-                         lng=input$OriginLon,
-                         icon = awesomeIcons(
-                           library = 'glyphicon',
-                           icon = "download",
-                           markerColor = "black"))}
-  p1
-  
+BreaksListReac=reactive({
+BreaksList=list()
+for(i in 1:Breaks){
+  if(i==1){
+    BreaksList[[i]]=c(min(TimeStoreReac()),min(TimeStoreReac())+TimeRangeReac()*i)
+  }else if(i==Breaks){
+    BreaksList[[i]]=c(min(TimeStoreReac())+TimeRangeReac()*(i-1),max(TimeStoreReac()))
+  }else{
+    BreaksList[[i]]=c(min(TimeStoreReac())+TimeRangeReac()*(i-1),min(TimeStoreReac())+TimeRangeReac()*i)
+  }}
+names(BreaksList)=paste0("Break_", 1:Breaks)
+
+BreaksList
 })
+BreakLegendReac=reactive({unique(unlist(BreaksListReac()))[1:(length(unique(unlist(BreaksListReac())))-1)]})
+
+timeRanking=function(x){
+  for(i in 1:Breaks){
+    if(x > BreaksListReac()[[i]][1] & x < BreaksListReac()[[i]][2]){
+      return(i)
+    }
+  }}
+
+RanksTimeReac=reactive({lapply(DayListv2Reac(), function(x) timeRanking(x$time))})
+
+#You can change the Pallete. Check RColorBrewer
+colorFun=colorNumeric(palette="BuPu", c(1:(Breaks*2)))
+#colorFun=colorNumeric(c("#00ffff","#008080","#3987C0","#08306B"), c(1:4))
+ColorReac=reactive({lapply(RanksTimeReac(), function(x) colorFun(x*2))})
+
+#Adding color to every route
+routeNumberReac=reactive({length(DayListv2Reac())})
+
+DayListv4Reac=reactive({
+  DayListv3=DayListv2Reac()
+for(routes in 1:routeNumberReac()){
+  DayListv3[[routes]]$Color=ColorReac()[[routes]]
 }
+  DayListv3
+})
+
+#output$DEBUG=renderPrint({DayListv4Reac()[[1]]})
+
+#PLOT
+#ALTERNATIVE MAPPING
+observeEvent(input$time, {
+ 
+p1=leafletProxy("gridmap") %>% clearShapes() %>% clearMarkers() %>%
+  clearMarkers() %>% clearPopups() %>% clearControls() %>%
+  addTiles() %>% addLayersControl(overlayGroups="Route Polylines",
+                                  options= layersControlOptions(collapsed=FALSE)) %>%
+  addProviderTiles(providers$OpenStreetMap) %>%
+  hideGroup("Route Polylines")
+
+for(routes in 1:routeNumberReac()){
+  #Add route lines
+  p1=addPolylines(p1,
+                  lat=DayListv4Reac()[[routes]]$lat,
+                  lng=DayListv4Reac()[[routes]]$lon,
+                  group="Route Polylines",
+                  color=DayListv4Reac()[[routes]]$Color,
+                  popup = paste(round(DayListv4Reac()[[routes]]$time, digits=2), "minutes;",
+                                round(DayListv4Reac()[[routes]]$distance, digits=2),"km"),
+                  highlight=highlightOptions(weight=5,
+                                             color="#ffff00",
+                                             dashArray="",
+                                             fillOpacity=0.7,
+                                             bringToFront=TRUE)
+  )
+  
+  #Points
+  p1=addCircles(p1,
+                lat=DayListv4Reac()[[routes]]$lat[length(DayListv4Reac()[[routes]]$lat)],
+                lng=DayListv4Reac()[[routes]]$lon[length(DayListv4Reac()[[routes]]$lon)],
+                color="black", fillColor="black",
+                group="Route Polylines", radius=50
+  )
+}
+
+#Add info marker         
+p1=addMarkers(p1, group="Route Polylines", lat=as.numeric(centralPointReac()[1]), 
+              lng=as.numeric(centralPointReac()[2]),
+              popup = paste("Central point is HERE", sep=""))
+
+
+p1= p1 %>% addLegend("bottomright", colors=colorFun(c(1:Breaks*2)),
+                     labels=paste0(">", round(BreakLegendReac(), digits=2),"min"),
+                     title="Travel Time Quantile")
+p1
+})
+ }
 
 
--- ui.R
+++ ui.R
@@ -1,18 +1,13 @@
 #Funciones fuera de Shiny App
 {
-library(RCurl)
-library(plyr)
-library(urltools)
-library(XML)
-library(leaflet)
-library(htmlwidgets)
-library(sp)
-library(stringr)
-library(shiny)
-library(tableHTML)
-library(shinyjs)
-library(shinydashboard)
-  }
+  library(RCurl)
+  library(XML)
+  library(leaflet)
+  library(stringr)
+  library(shiny)
+  library(shinyjs)
+  library(shinydashboard)
+}
 #Client Side
 
 ui= fluidPage(
@@ -20,15 +15,21 @@ ui= fluidPage(
   sidebarLayout(
     sidebarPanel(
   
-  textInput(inputId="Date", label="Date", value=Sys.time()+86400),
+  #Input date in future. System day + 1 day is used to prevent a past time
+  textInput(inputId="Date", label="Date", value=Sys.time()),
+  textInput(inputId="TimeZone", label="Time Zone in UTC", value=-1),
+  textOutput("DEBUG"),
   
   fluidRow(
     box(width = 10, title="Origin point:",
         splitLayout(
-          numericInput(inputId="OriginLat", label="Latitude", value=-33.4516906, min=-90, max=90, step=0.5),
-          numericInput(inputId="OriginLon", label="Longitude", value=-70.66742690000001, min=-180, max=180, step=0.5)
+          numericInput(inputId="centralPointLat", label="Latitude", 
+                       value=-33.4516906, min=-90, max=90, step=0.5),
+          numericInput(inputId="centralPointLon", label="Longitude", 
+                       value=-70.66742690000001, min=-180, max=180, step=0.5)
         ),
-        helpText(a("Find Latitude and Longitude values for specific locations here!", target="_blank", href="https://www.gps-coordinates.net/"))
+        helpText(a("Find Latitude and Longitude values for specific locations here!", 
+                   target="_blank", href="https://www.gps-coordinates.net/"))
         
     )
   ),
@@ -38,15 +39,16 @@ ui= fluidPage(
   fluidRow(
     box(width = 10, title="Optional: Grid size", footer="Dont change this, ideally",
         splitLayout(
-          numericInput(inputId="PointMatrixLat", label="Latitude", value=0.5, min=0.1, max=1),
-          numericInput(inputId="PointMatrixLon", label="Longitude", value=0.8, min=0.1, max=1)
+          numericInput(inputId="LatitudeAxisLength", label="Latitude", value=0.5, min=0.1, max=1),
+          numericInput(inputId="LongitudeAxisLength", label="Longitude", value=0.8, min=0.1, max=1)
         )
     )
   ),
   numericInput(inputId="Puntos", label="Points", value=100, min=100, max=500),
   
   textInput(inputId="Key", label="Google API Key"),
-  helpText(a("Get your Google Maps API Key here!", target="_blank", href="https://developers.google.com/maps/documentation/javascript/get-api-key?hl=EN"))
+  helpText(a("Get your Google Maps API Key here!", target="_blank", 
+             href="https://developers.google.com/maps/documentation/javascript/get-api-key?hl=EN"))
     ),
   mainPanel(
   titlePanel("Instructions:"),
